c726ba3dcdf5fec1aa58e6d760f3661b75663064
shreyaspatil2712 <87693150+shreyaspatil2712@users.noreply.github.com>
2022-11-15 06:14:28 +0530

    Update cd2.c

--- cd2.c
+++ cd2.c
@@ -1051,4 +1051,156 @@ void search()
     }     
           
 }  
+               
+ ________________________________________________________________________________
+               STACK 
+               
+ #include <stdio.h>
+
+int MAXSIZE = 8;       
+int stack[8];     
+int top = -1;            
+
+int isempty() {
+
+   if(top == -1)
+      return 1;
+   else
+      return 0;
+}
+   
+int isfull() {
+
+   if(top == MAXSIZE)
+      return 1;
+   else
+      return 0;
+}
+
+int peek() {
+   return stack[top];
+}
+
+int pop() {
+   int data;
+	
+   if(!isempty()) {
+      data = stack[top];
+      top = top - 1;   
+      return data;
+   } else {
+      printf("Could not retrieve data, Stack is empty.\n");
+   }
+}
+
+int push(int data) {
+
+   if(!isfull()) {
+      top = top + 1;   
+      stack[top] = data;
+   } else {
+      printf("Could not insert data, Stack is full.\n");
+   }
+}
+
+int main() {
+   // push items on to the stack 
+   push(3);
+   push(5);
+   push(9);
+   push(1);
+   push(12);
+   push(15);
+
+   printf("Element at top of the stack: %d\n" ,peek());
+   printf("Elements: \n");
+
+   // print stack data 
+   while(!isempty()) {
+      int data = pop();
+      printf("%d\n",data);
+   }
+
+   printf("Stack full: %s\n" , isfull()?"true":"false");
+   printf("Stack empty: %s\n" , isempty()?"true":"false");
+   
+   return 0;
+}
+               
+ ___________________________________________________________________________________________________________________________________
+               QUE
+// Queue implementation in C
+
+#include <stdio.h>
+#define SIZE 5
+
+void enQueue(int);
+void deQueue();
+void display();
+
+int items[SIZE], front = -1, rear = -1;
+
+int main() {
+  //deQueue is not possible on empty queue
+  deQueue();
+
+  //enQueue 5 elements
+  enQueue(1);
+  enQueue(2);
+  enQueue(3);
+  enQueue(4);
+  enQueue(5);
+
+  // 6th element can't be added to because the queue is full
+  enQueue(6);
+
+  display();
+
+  //deQueue removes element entered first i.e. 1
+  deQueue();
+
+  //Now we have just 4 elements
+  display();
+
+  return 0;
+}
+
+void enQueue(int value) {
+  if (rear == SIZE - 1)
+    printf("\nQueue is Full!!");
+  else {
+    if (front == -1)
+      front = 0;
+    rear++;
+    items[rear] = value;
+    printf("\nInserted -> %d", value);
+  }
+}
+
+void deQueue() {
+  if (front == -1)
+    printf("\nQueue is Empty!!");
+  else {
+    printf("\nDeleted : %d", items[front]);
+    front++;
+    if (front > rear)
+      front = rear = -1;
+  }
+}
+
+// Function to print the queue
+void display() {
+  if (rear == -1)
+    printf("\nQueue is Empty!!!");
+  else {
+    int i;
+    printf("\nQueue elements are:\n");
+    for (i = front; i <= rear; i++)
+      printf("%d  ", items[i]);
+  }
+  printf("\n");
+}               
+               
+               
+               
 
